44c36dd00081338682a6330f28ec7a679ca12fa2
syamgith <syamgithksudheesh@gmail.com>
2020-08-16 16:46:59 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/syamgith/bloc_login/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.syamgith.bloc_login
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+          SizedBox(
+            height: 16.0,
+          ),
+          submitButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration:
+          InputDecoration(hintText: 'you@example.com', labelText: 'Email'),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(hintText: 'password', labelText: 'Password'),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+        child: Text('Submit'), color: Colors.blue, onPressed: () {});
+  }
+}

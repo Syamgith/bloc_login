dd2b85a60ac8de9a647dbb259c9eb078383c4104
syamgith <syamgithksudheesh@gmail.com>
2020-08-18 00:45:13 +0530

    rxdard combinestream2

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:rxdart/rxdart.dart';
+
 import 'validators.dart';
 
 class Bloc extends Object with Validators {
@@ -8,6 +10,9 @@ class Bloc extends Object with Validators {
 
   Stream<String> get email => _email.stream.transform(validateEmail);
   Stream<String> get password => _password.stream.transform(validatePassword);
+
+  Stream<bool> get submit =>
+      CombineLatestStream.combine2(email, password, (e, p) => true);
 
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _password.sink.add;
